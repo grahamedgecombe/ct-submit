3a7c00f5da34c35f0338ae55669ad33528fa318e
Graham Edgecombe <gpe@grahamedgecombe.com>
2015-04-25 18:17:34 +0100

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,53 @@
+ct-submit
+=========
+
+Introduction
+------------
+
+`ct-submit` is a program that submits X.509 certificate chains to
+[Certificate Transparency][ct] log servers. It returns the Signed Certificate
+Timestamp structure in a format suitable for use with Apache's
+[mod\_ssl\_ct][apache] module and [nginx-ct][nginx].
+
+Building
+--------
+
+`ct-submit` is written in [Go][go]. Just run `go build` to build it.
+
+Usage
+-----
+
+`ct-submit` takes a single argument - the URL of the log server (without the
+leading `https://`). It reads the certificate chain in PEM format from `stdin`.
+The leaf certificate should be the first certificate in the chain, followed by
+any intermediate certificates and, optionally, the root certificate.
+
+The encoded SCT structure is written in binary to `stdout`.
+
+The following example demonstrates submitting the chain in `gpe.pem` to
+Google's pilot log server. The SCT is written to `gpe.sct`, which is in a format
+suitable for use with Apache's mod\_ssl\_ct module and nginx-ct.
+
+    $ ./ct-submit ct.googleapis.com/pilot <gpe.pem >gpe.sct
+    $ xxd gpe.sct
+    00000000: 00a4 b909 90b4 1858 1487 bb13 a2cc 6770  .......X......gp
+    00000010: 0a3c 3598 04f9 1bdf b8e3 77cd 0ec8 0ddc  .<5.......w.....
+    00000020: 1000 0001 4bc7 e617 c800 0004 0300 4830  ....K.........H0
+    00000030: 4602 2100 b9fe e206 f0f5 f600 93d5 e04c  F.!............L
+    00000040: d2fd 75c9 e1fc a5c8 4812 a8b7 bc2c eb0c  ..u.....H....,..
+    00000050: ee16 1fe9 0221 008a 5974 e1b6 a0e0 281a  .....!..Yt....(.
+    00000060: 61e8 3447 895f 7ad4 2f70 f528 6133 a445  a.4G._z./p.(a3.E
+    00000070: 4fd4 ab60 ba36 db                        O..`.6.
+    $ 
+
+License
+-------
+
+`ct-submit` is available under the terms of the ISC license, which is similar to
+the 2-clause BSD license. See the `LICENSE` file for the copyright information
+and licensing terms.
+
+[ct]: http://www.certificate-transparency.org/
+[apache]: https://httpd.apache.org/docs/trunk/mod/mod_ssl_ct.html
+[nginx]: https://github.com/grahamedgecombe/nginx-ct
+[go]: https://golang.org/
---
+++ ct-submit.go
@@ -0,0 +1,171 @@
+// Copyright (c) 2015 Graham Edgecombe <gpe@grahamedgecombe.com>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type addChain struct {
+	Chain []string `json:"chain"`
+}
+
+type signedCertificateTimestamp struct {
+	Version    uint8  `json:"sct_version"`
+	LogID      string `json:"id"`
+	Timestamp  int64  `json:"timestamp"`
+	Extensions string `json:"extensions"`
+	Signature  string `json:"signature"`
+}
+
+func (sct signedCertificateTimestamp) Write(w io.Writer) error {
+	// Version
+	if err := binary.Write(w, binary.BigEndian, sct.Version); err != nil {
+		return err
+	}
+
+	// LogID
+	bytes, err := base64.StdEncoding.DecodeString(sct.LogID)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	// Timestamp
+	if err := binary.Write(w, binary.BigEndian, sct.Timestamp); err != nil {
+		return err
+	}
+
+	// Extensions
+	bytes, err = base64.StdEncoding.DecodeString(sct.Extensions)
+	if err != nil {
+		return err
+	}
+
+	length := len(bytes)
+	if length > 65535 {
+		return errors.New("extensions are too long")
+	}
+
+	if err := binary.Write(w, binary.BigEndian, uint16(length)); err != nil {
+		return err
+	}
+
+	_, err = w.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	// Signature
+	bytes, err = base64.StdEncoding.DecodeString(sct.Signature)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func main() {
+	// parse args
+	if len(os.Args) != 2 {
+		fmt.Fprintf(os.Stderr, `usage: ct-submit [-v] <log server>
+
+ct-submit reads a PEM-encoded X.509 certificate chain from stdin and submits it
+to the given Certificate Transparency log server. The Signed Certificate
+Timestamp structure returned by the log server is written to stdout in binary.
+
+The leaf certificate should be the first certificate in the chain, followed by
+any intermediate certificates and, optionally, the root certificate.
+
+The signature of the SCT is not verified.
+`)
+		os.Exit(1)
+	}
+
+	logServer := os.Args[1]
+
+	// read certificate chain from stdin
+	in, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	msg := addChain{}
+	for {
+		block, remaining := pem.Decode(in)
+		in = remaining
+
+		if block == nil {
+			break
+		}
+
+		if block.Type != "CERTIFICATE" {
+			continue
+		}
+
+		msg.Chain = append(msg.Chain, base64.StdEncoding.EncodeToString(block.Bytes))
+	}
+
+	// send add-chain message to the log
+	payload, err := json.Marshal(msg)
+	if err != nil {
+		panic(err)
+	}
+
+	url := "https://" + logServer + "/ct/v1/add-chain"
+	response, err := http.Post(url, "application/json", bytes.NewReader(payload))
+	if err != nil {
+		panic(err)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		panic(fmt.Errorf("unexpected status %s from log server", response.Status))
+	}
+
+	// decode JSON SCT structure
+	payload, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	sct := signedCertificateTimestamp{}
+	if err = json.Unmarshal(payload, &sct); err != nil {
+		panic(err)
+	}
+
+	// write binary SCT structure to stdout
+	if err = sct.Write(os.Stdout); err != nil {
+		panic(err)
+	}
+}
